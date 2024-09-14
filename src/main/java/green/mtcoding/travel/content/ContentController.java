545c63446ed316112260752d5907be0c889cd777
kimsoapsoap <zerfa@naver.com>
2024-09-14 15:42:45 +0900

    feat: 핫플 리스트 출력

--- src/main/java/green/mtcoding/travel/content/ContentController.java
+++ src/main/java/green/mtcoding/travel/content/ContentController.java
@@ -44,6 +44,7 @@ public class ContentController {
     /*           region-end             */
 
 
+
     /*           hotPlace-start             */
     @GetMapping("/hotplace")
     public String hotPlace(
