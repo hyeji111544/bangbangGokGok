b975166ca8b68776cf983992ea65f504f6841092
eunmouse <ejha1208@gmail.com>
2024-09-20 14:36:43 +0900

    feat: festival_list
    # Conflicts:
    #	build.gradle
    #	src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfo.java
    #	src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoController.java
    #	src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoRepository.java
    #	src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoService.java
    #	src/main/resources/db/data.sql
    #	src/main/resources/static/css/festival-main.css
    #	src/main/resources/templates/festival/festival.mustache
    #	src/main/resources/templates/layout/header.mustache

--- src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoController.java
+++ src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoController.java
@@ -1,8 +1,11 @@
 package green.mtcoding.travel.festivalInfo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +26,19 @@ public class FestivalInfoController {
     /*           hotPlace-end             */
 
     /*           festival-start             */
+    // 쿼리스트링 전달
+    // http://localhost:8080/festival/main?
+    @GetMapping("/festival/main")
+    public String festival(HttpServletRequest request) {
+        List<FestivalInfoResponse.FestivalMainDTO> festivalInfoList = festivalInfoService.목록보기();
+        request.setAttribute("models", festivalInfoList);
+        return "/festival/festival";
+    }
+
+    @GetMapping("/festival/{contentid}")
+    public String festivalDetail() {
+        return "";
+    }
     /*           festival-end             */
 
     /*           info-start             */
--- src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoService.java
+++ src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoService.java
@@ -2,9 +2,12 @@ package green.mtcoding.travel.festivalInfo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
-@Controller
+import java.util.List;
+
 @RequiredArgsConstructor
+@Service
 public class FestivalInfoService {
 
     private final FestivalInfoRepository festivalInfoRepository;
@@ -22,6 +25,19 @@ public class FestivalInfoService {
     /*           hotPlace-end             */
 
     /*           festival-start             */
+    public List<FestivalInfoResponse.FestivalMainDTO> 목록보기() {
+        List<FestivalInfoResponse.FestivalMainDTO> festivalInfoList = festivalInfoRepository.findAll();
+
+        // index 값 설정
+        for (int i = 0; i < festivalInfoList.size(); i++) {
+            FestivalInfoResponse.FestivalMainDTO dto = festivalInfoList.get(i);
+            dto.setIndex(i);
+            // 첫 번째 항목에 active 값 설정
+            dto.setActive(i == 0); // 0 일때만 true 반환
+        }
+
+        return festivalInfoList;
+    }
     /*           festival-end             */
 
     /*           info-start             */
