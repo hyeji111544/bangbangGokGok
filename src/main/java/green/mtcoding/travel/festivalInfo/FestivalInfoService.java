04a37dcb08a077299acfc5e36207143d40f74e6d
eunmouse <ejha1208@gmail.com>
2024-09-28 20:06:06 +0900

    fix: festival main/detail (#51)
    * 작업중
    
    * 작업완/검증필
    
    * 축제 수정중
    
    # Conflicts:
    #	src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoService.java
    
    * fix: festival 메인/상세 전면 수정

--- src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoService.java
+++ src/main/java/green/mtcoding/travel/festivalInfo/FestivalInfoService.java
@@ -44,24 +44,23 @@ public class FestivalInfoService {
     }
     @Transactional
     public FestivalInfoResponse.FestivalDetailDTO saveFestivalData(FestivalInfoRequest.SaveDTO saveDTO) {
-        // 1. 조회 후 없으면 등록
+        // 1. festival_tb 조회 후 없는 정보면 등록 후 리턴 / 있는 정보면 있는 정보 리턴
         try {
             FestivalInfo festivalInfoPS = festivalInfoRepository.findByContentId(saveDTO.getContentId());
+            System.out.println("festival_tb 에 있는 데이터로, DB 에서 데이터 가져옴");
+            return new FestivalInfoResponse.FestivalDetailDTO(festivalInfoPS);
         } catch (Exception e) {
             festivalInfoRepository.save(saveDTO.toEntity()); // 담궈지는 순간, 만들어진다.
             FestivalInfo festivalInfoPS = festivalInfoRepository.findByContentId(saveDTO.getContentId());
+            System.out.println("festival_tb 에 없는 데이터로, DB 에 저장하고 저장한 데이터 가져옴");
             return new FestivalInfoResponse.FestivalDetailDTO(festivalInfoPS);
         }
-        return null;
     }
     @Transactional
     public void saveFestivalDataForScrap(FestivalInfoRequest.SaveDTO saveDTO) {
-        // 1. 조회 후 없으면 등록
+        // 1. 조회 후 content_TB 에 없는 축제 정보면 등록
         try {
-           if (contentRepository.findByContentTypeIdAndContentId(saveDTO.getContentTypeId(), saveDTO.getContentId()) == null) {
-               System.out.println("null 임, Content_TB 에 없는 축제정보임");
-               contentRepository.save(saveDTO.getContentId(), saveDTO.getAddr1(), saveDTO.getAreaCode(), saveDTO.getContentTypeId(), saveDTO.getFirstImage(), saveDTO.getSigunguCode(), saveDTO.getTitle());
-           } else {
+           if (contentRepository.findByContentTypeIdAndContentId(saveDTO.getContentTypeId(), saveDTO.getContentId()) != null) {
                System.out.println("Content_TB 에 있는 축제정보, 스크랩에 문제 없음");
            }
         } catch (Exception e) {
